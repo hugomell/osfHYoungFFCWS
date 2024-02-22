aabb33cae1e625ffa48a8ef83d2ed2afaab375e3
Hugo Mell <hugo76m@hotmail.com>
2024-02-22 09:35:24 +0100

    feat: add a targets script to package installed files

---
+++ inst/_targets.R
@@ -0,0 +1,39 @@
+# Load packages required to define the pipeline:
+library(targets)
+library(tarchetypes) # Load other packages as needed.
+
+# Set target options:
+tar_option_set(packages = "osfHYoungFFCWS")
+
+# Replace the target list below with your own:
+list(
+  tar_target(
+    name = sim_data_baseline,
+    command = simulate_dataset(model_type = "baseline")
+  ),
+  tar_target(
+    name = sim_data_mediation,
+    command = simulate_dataset(model_type = "mediation")
+  ),
+  tar_target(
+    name = sim_data_moderation,
+    command = simulate_dataset(model_type = "moderation")
+  ),
+  tar_target(
+    name = fit_baseline,
+    command = brms_fit(sim_data_baseline, model_type = "baseline")
+  ),
+  tar_target(
+    name = fit_mediation,
+    command = brms_fit(sim_data_mediation, model_type = "mediation")
+  ),
+  tar_target(
+    name = fit_moderation,
+    command = brms_fit(sim_data_moderation, model_type = "moderation")
+  ),
+  # Generate quarto report
+  tar_quarto(
+        report,
+        path = "qmd/simu-pipeline.qmd"
+    )
+)
