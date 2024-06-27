ddbe95f70814512c2077bfdebd4b518cef252445
Hugo Mell <hugo76m@hotmail.com>
2024-06-27 19:34:45 +0200

    fix: typo in utils function

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,8 @@
 #' Copy files to run demonstration pipeline in root
 #' @export
 setup_demo_pipeline <- function() {
-  tar_script <- system.file("_targets.R", package = "osfhyoungffcws")
-  tar_yaml <- system.file("_targets.yaml", package = "osfhyoungffcws")
+  tar_script <- system.file("_targets.R", package = "osfHYoungFFCWS")
+  tar_yaml <- system.file("_targets.yaml", package = "osfHYoungFFCWS")
   tar_rmd  <- system.file(
     "data-simulation-and-parameter-recovery-with-brms.rmd",
     package = "osfhyoungffcws")
