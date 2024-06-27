bd518c91c7e4edd7da5f31de4e697cc217f4c02a
Hugo Mell <hugo76m@hotmail.com>
2024-06-27 18:28:03 +0200

    refactor: keep only files useful for gitpod demo

---
+++ test_demo_pipeline.R
@@ -0,0 +1,6 @@
+# Run the following line in the R console to run the pipeline.
+# Note that the first run will take some time to complete.
+osfHYoungFFCWS::run_demo_pipeline(gitpod = TRUE)
+
+# Check the output report:
+# - Right clik -> Download on Data
